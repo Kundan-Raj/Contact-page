a00db02fdc30cf8e7a10ab2bb3c1630448c1f48f
Kundan Raj <kundanr387@gmail.com>
2020-10-05 15:34:24 +0530

    updated code to get  rid of name, value type error

--- test1/views.py
+++ test1/views.py
@@ -11,15 +11,33 @@ from .models import Contactdb
 def indexView(request):
     contactlist = Contactdb.objects.all()
     context = {'contactlist':contactlist}
-    return render(request,'home.html',context)
+    return render(request,'home.html')
 
+
+#def registerView(request):
+ #   form= ContactForm(request.POST or None)
+  #  if form.is_valid():
+   #     name = form.cleaned_data['name']
+    #    form.save()
+    #context= {'form': form}
+    #return render(request, 'contact/register.html', context)
+    
 
 def registerView(request):
+    # request should be ajax and method should be POST.
+    if request.is_ajax and request.method == "POST":
+        # get the form data
+        form = ContactForm(request.POST)
+        # save the data and after fetch the object in instance
+        if form.is_valid():
+            instance = form.save()
+            # serialize in new friend object in json
+            ser_instance = serializers.serialize('json', [ instance, ])
+            # send to client side.
+            return JsonResponse({"instance": ser_instance}, status=200)
+        else:
+            # some form errors occured.
+            return JsonResponse({"error": form.errors}, status=400)
 
-    form= ContactForm(request.POST or None)
-    if form.is_valid():
-        name = form.cleaned_data['name']
-        form.save()
-    context= {'form': form}
-    return render(request, 'contact/register.html', context)
-    
+    # some error occured
+    return JsonResponse({"error": ""}, status=400)
