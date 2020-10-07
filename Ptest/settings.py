a86565a81ca6195e1ac742f86600378128deb566
Kundan Raj <kundanr387@gmail.com>
2020-10-07 09:54:26 +0530

    static root removed from settings

--- Ptest/settings.py
+++ Ptest/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 
 #Static files (style1, bs, styles)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+#PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+#STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
+#STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
